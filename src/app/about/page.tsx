5d56b6dfd70c088af94ce014a1d53935fead5e30
Shehab Waleed <shehabwaleedd@gmail.com>
2024-08-21 19:30:51 +0300

    refactor: Improve video player functionality and responsiveness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -70,7 +70,7 @@ const AboutPage = () => {
                 </video>
             </section>
             <div ref={contentRef} className={styles.container}>
-                <div className={styles.centered}>
+                <div className={`${styles.centered} ${styles.topP}`}>
                     <h2> Integrity, Grit & Imagination </h2>
                     <p> We are a global creative studio dedicated to the realm of architecture, partnering with visionary businesses, funds, and developers to shape the future through our relentless pursuit of excellence. </p>
                     <p> Our consultancy delivers bespoke solutions covering brand strategy, content, and production for a diverse array of architectural and design firms. We&apos;ve cultivated a unique culture comprising influential auteurs from the worlds of film and photography. </p>
@@ -82,7 +82,7 @@ const AboutPage = () => {
                 <section className={styles.lower}>
                     <Image src="/assets/aboutPage/img2.png" alt="About" width={1920} height={1080} />
                 </section>
-                <div className={styles.rightText}>
+                <div className={`${styles.list} ${styles.finalBottom}`}>
                     <h2> A work of wonder </h2>
                     <p>We prioritize clarity of message and cost certainty in content creation, delivering measurable results with a passion for detail and authenticity. Our commitment to inclusivity, equity, and sustainability underpins everything we do.</p>
                     <p>Through our work, we create a future where seventh art, literature, music, and architecture converge to make a meaningful impact, leaving a legacy that resonates for generations to come.</p>
