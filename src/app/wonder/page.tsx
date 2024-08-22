b601767d978c7f72a457671d99dca88310e5fa1b
Shehab Waleed <shehabwaleedd@gmail.com>
2024-08-22 20:22:28 +0300

    clients refining

--- src/app/wonder/page.tsx
+++ src/app/wonder/page.tsx
@@ -78,7 +78,7 @@ const WonderPage = () => {
             <div ref={contentRef} className={styles.container}>
                 <section className={`${styles.centered} ${styles.topP}`}>
                     <h2 className={styles.firstH1}>Wonder With Us</h2>
-                    <p>By accessing this page, you&apos;ve unveiled our most cherished project - one that embodies the true DNA of Wonderful.</p>
+                    <p>By accessing this page, you&apos;ve unveiled our most cherished project - one that embodies the true DNA of Wonderwil.</p>
                     <p>Our creative studio is not only a specialist consultant, trusted to create brand strategy, branding, content, and production for many of the most high-profile projects in the world.</p>
                     <p>We are also committed to the advocacy, development, and advancement of community-powered public spaces for social good.</p>
                     <p>And we need you to achieve this together.</p>
@@ -106,7 +106,7 @@ const WonderPage = () => {
                     <Image src="/assets/wonderPage/img3.png" alt="Wonder Page Image" width={1920} height={1080} />
                 </section>
                 <div className={`${styles.list} ${styles.dynamic}`}>
-                    <h2 style={{ color: "var(--title-color)" }}>The living canvas</h2>
+                    <h2 style={{ color: "#9d8e85" }}>The living canvas</h2>
                     <p>Intimate, thought-provoking, visceral and poetic storytelling is the groundwork of our content ethos. A masterfully crafted visual narrative focuses on the relationship of public space to people.</p>
                     <p>Through video (a short film about the public space directed by a cinema auteur), audio (bottles of ambient sounds that act as geographical artifacts), and archival footage, we aim to illustrate the power of public spaces as platforms for community development, creating a sense of authentic human conversation among the democratic places of the world.</p>
                     <p>Supplementary visuals range from historical imagery, interactions between spaces from the past and present, and data visualization that can move us to re-dream and promote the future of our cities and environment.</p>
@@ -116,8 +116,8 @@ const WonderPage = () => {
                         <Image src="/assets/wonderPage/img4.png" alt="Wonder Page Image" width={1920} height={1080} />
                     </div>
                 </section>
-                <div className={`${styles.centered} ${styles.dark}`}>
-                    <h2>The pulse of place</h2>
+                <div className={`${styles.centered} ${styles.differentColor}`}>
+                    <h2 >The pulse of place</h2>
                     <p>A new paradigm of interaction for the discovery and exploration of the public realm will take shape through the development of an immersive map</p>
                     <p>The Wonder Map will serve as guiding compass, allowing us to navigate partnering public spaces and granting access to its free-standing library of vérité documentaries and a constantly refreshed selection of special features.</p>
                     <p>The Wonder Map is designed to stimulate the ongoing dialogue about the vital life force of cities, nature, beauty and history.</p>
@@ -128,8 +128,8 @@ const WonderPage = () => {
                         <Image src="/assets/wonderPage/img5.png" alt="Wonder Page Image" width={1920} height={1080} />
                     </div>
                 </section>
-                <div className={`${styles.list} ${styles.finalBottom}`}>
-                    <h2 style={{ color: "var(--title-color)" }}>Awe - gora</h2>
+                <div className={`${styles.list} ${styles.finalBottom} ${styles.differentColor}`}>
+                    <h2 style={{ color: "var(--title-color)" }}>Awe-gora</h2>
                     <p>Our distribution model will include international cultural exhibitions, such as the Venice Biennale, architectural firms, art exhibitions, public installations, education centers, local institutions, and performance spaces for talks and panels.</p>
                     <p>Wonderwil is the culmination of a lifetime&apos;s vision. At the heart of our work is focus community, meaningful shared experiences, and lasting impact. Our projects will stand as beacons of hope and renewal through continual exploration and reinterpretation, bringing vitality to the arts and public spaces.</p>
                     <p>We invite you to wonder with us.</p>
